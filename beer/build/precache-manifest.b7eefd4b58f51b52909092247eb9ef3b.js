c9abdbd33dc356f0f61ee0811c7b8a6d6e6eaa71
Vethavear <adrian.bury@gmail.com>
2020-10-19 15:43:25 +0200

    fixed z-index bug with searcher loop icon on mobile, pushed to netlify

---
+++ beer/build/precache-manifest.b7eefd4b58f51b52909092247eb9ef3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31228dee2f79ed7cb2cb70cecf5b545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb35a9ab3e05e5ce83d",
+    "url": "/static/css/main.4523e158.chunk.css"
+  },
+  {
+    "revision": "50a4e8e2dfc22c5091da",
+    "url": "/static/js/2.9059baa4.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.9059baa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb35a9ab3e05e5ce83d",
+    "url": "/static/js/main.e1c08d5c.chunk.js"
+  },
+  {
+    "revision": "195ed6df6314781b184a",
+    "url": "/static/js/runtime-main.93b661a8.js"
+  },
+  {
+    "revision": "996bfe5e23b5fdea76a3922ab67223b8",
+    "url": "/static/media/beerImg.996bfe5e.jpg"
+  }
+]);
